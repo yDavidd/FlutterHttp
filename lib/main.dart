b0929c13fa2dba1af728de6aa9f9912af1785fe7
yDavidd <132359951+yDavidd@users.noreply.github.com>
2024-05-16 11:51:48 -0300

    Projeto HTTP

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_verbos_http/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
